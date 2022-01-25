ef53f92eb1a3d0957fc3044fff7e06c9d7629b69
reneeb <info@perl-services.de>
2022-01-25 11:43:42 +0100

    initial commit

---
+++ Kernel/System/DynamicField/Driver/RoText.pm
@@ -0,0 +1,44 @@
+# --
+# Copyright (C) 2022 Perl-Services.de, https://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package Kernel::System::DynamicField::Driver::RoText;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+
+use parent qw(Kernel::System::DynamicField::Driver::BaseText);
+
+=head1 NAME
+
+Kernel::System::DynamicField::Driver::RoText
+
+=head1 DESCRIPTION
+
+DynamicFields Text Driver delegate
+
+=head1 PUBLIC INTERFACE
+
+This module implements the public interface of L<Kernel::System::DynamicField::Backend>.
+Please look there for a detailed reference of the functions.
+
+=cut
+
+sub EditFieldRender {
+    my ( $Self, %Param ) = @_;
+
+    my $Data = $Self->SUPER::EditFieldRender( %Param );
+
+    $Data->{Field} =~ s{<input\K}{ readonly}xms;
+
+    return $Data;
+}
+
+1;
+}
