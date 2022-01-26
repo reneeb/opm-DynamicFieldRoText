d24cbcea3c011cd13c4f1a8c3c09506343023889
reneeb <info@perl-services.de>
2022-01-26 14:02:01 +0100

    add backend for readonly textarea

---
+++ Kernel/System/DynamicField/Driver/RoTextArea.pm
@@ -0,0 +1,45 @@
+# --
+# Copyright (C) 2022 Perl-Services.de, https://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package Kernel::System::DynamicField::Driver::RoTextArea;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+
+use parent qw(Kernel::System::DynamicField::Driver::TextArea);
+
+=head1 NAME
+
+Kernel::System::DynamicField::Driver::RoTextArea
+
+=head1 DESCRIPTION
+
+DynamicFields Text Driver delegate
+
+=head1 PUBLIC INTERFACE
+
+This module implements the public interface of L<Kernel::System::DynamicField::Backend>.
+Please look there for a detailed reference of the functions.
+
+=cut
+
+no warnings 'redefine';
+
+sub EditFieldRender {
+    my ( $Self, %Param ) = @_;
+
+    my $Data = $Self->SUPER::EditFieldRender( %Param );
+
+    $Data->{Field} =~ s{<textarea\K}{ readonly}xms;
+
+    return $Data;
+}
+
+1;
